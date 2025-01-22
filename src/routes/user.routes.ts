7a0633f10bf72a5a8cd9bd29508331cd0bd5e662
KryssNa <krishna.kryss@gmail.com>
2025-01-22 08:54:31 +0545

    user routes modified

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,7 +4,7 @@ import { UserController } from "../controllers/user.controller";
 import { authenticate } from "../middlewares/auth";
 import { authorize } from "../middlewares/authorize";
 import { validateRequest } from "../middlewares/validate";
-import { createUserSchema, loginSchema } from "../schemas/user.schema";
+import { createUserSchema, loginSchema, updateUserSchema } from "../schemas/user.schema";
 import { UserRole } from "../types/user.types";
 
 const router = Router();
@@ -20,18 +20,20 @@ router.post("/logout", authenticate, UserController.logout);
 router.post("/verify-access-token", UserController.verifyAccessToken);
 
 // Protected routes
-// router.use(authenticate);
+router.use(authenticate);
 
 // User routes
-// router.get("/profile", UserController.);
-// router.patch(
-//   "/profile",
-//   validateRequest(createUserSchema),
-//   UserController.updateProfile
-// );
+router.get("/profile", UserController.getUserProfile);
+router.patch(
+    "/profile",
+    validateRequest(updateUserSchema),
+    UserController.updateUserProfile
+);
 
 // Admin routes
 router.get("/users", authorize([UserRole.ADMIN]), UserController.getUsers);
+
+router.patch("/users/:id", authorize([UserRole.ADMIN]), UserController.updateUserProfile);
 
 router.get('/admin/users', authenticate, UserController.getUsers);
 
