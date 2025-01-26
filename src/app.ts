997529f039979838785e64a89dc529e76b03272f
KryssNa <krishna.kryss@gmail.com>
2025-01-26 07:56:20 +0545

    new routes added

--- src/app.ts
+++ src/app.ts
@@ -9,14 +9,15 @@ import { errorHandler } from "./middlewares/error";
 import perfumeRoutes from "./routes/ai-recommendation.routes";
 import cartRoutes from "./routes/cart.routes";
 import categoryRoutes from "./routes/category.routes";
+import checkoutRoutes from "./routes/checkout.routes";
 import contactRoutes from "./routes/contact.routes";
+import orderRoutes from "./routes/order.routes";
+import paymentRoutes from "./routes/payment.routes";
 import productRoutes from "./routes/product.routes";
 import uploadsRoutes from "./routes/uploads.routes";
 import authRoutes from "./routes/user.routes";
 import variantsRoutes from "./routes/variant.routes";
 import wishlistRoutes from "./routes/wishlist.routes";
-import checkoutRoutes from "./routes/checkout.routes";
-import orderRoutes from "./routes/order.routes";
 
 const app = express();
 
@@ -27,10 +28,9 @@ config({
 
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
 // Middleware
 app.use(cors({
-  origin: true,
+  origin: "http://localhost:3000",
   credentials: true, allowedHeaders: 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
   methods: 'GET, POST, PATCH, PUT, POST, DELETE, OPTIONS',
   optionsSuccessStatus: 200,
@@ -39,7 +39,6 @@ app.use(cors({
 app.use(helmet());
 app.use(compression());
 app.use(morgan("dev"));
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan("dev"));
@@ -49,7 +48,16 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 // Serve static files from uploads directory
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '../uploads'), {
+  setHeaders: (res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  },
+}));
+
+app.get('/test-image', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, 'uploads/products/images/49bfd2d3-8abb-49f2-9339-746978d7270a.png'));
+});
+
 
 // Routes
 app.use("/api/v1/perfume", perfumeRoutes);
@@ -64,6 +72,7 @@ app.use("/api/v1/wishlist", wishlistRoutes);
 app.use("/api/v1/cart", cartRoutes);
 app.use("/api/v1/checkout", checkoutRoutes);
 app.use("/api/v1/orders", orderRoutes);
+app.use("/api/v1/payment", paymentRoutes);
 
 // Error handling
 app.use(errorHandler);
