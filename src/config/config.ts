0a84909e21d874cd3a2c19736f218e757fbd0550
KryssNa <krishna.kryss@gmail.com>
2025-01-31 06:34:44 +0545

    added new config files

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,5 +7,9 @@ export const config = {
     EMAIL_USER: process.env.EMAIL_USER,
     EMAIL_PASS: process.env.EMAIL_PASS,
     EMAIL_FROM: process.env.EMAIL_FROM || 'noreply@sugandha.com',
-    CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:3000'
+    CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:3000',
+    CORS_ALLOWED_ORIGINS: process.env.CORS_ALLOWED_ORIGINS || ['http://localhost:3000', 'https://localhost:3000'],
+
+    // New configurations mongoDB
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/sugandha',
   }
